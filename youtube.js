1078292f07d3ed4c5c4f6f9ce44017f85653ecbd
Noah G <NoahBGlasser@gmail.com>
2019-03-11 14:58:37 -0700

    new file

---
+++ youtube.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+  $("#submit").on("click", function(event) {
+      event.preventDefault()
+      var foodName = $("#ingredients").val()
+      //var foodName = $(this).val()
+      console.log(foodName)
+      var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=how+to+cook+" + foodName + "&key=AIzaSyDLkMMIuBWt42J1WLEhQ-_pQx0bb9ZmeKo"
+          $.ajax({
+          url: queryURL,
+          method: "GET"
+      })
+  
+      .then(function(response) {
+      var videoId = response.items[0].id.videoId
+      console.log(videoId)
+          $("#video").attr("src", "https://www.youtube.com/embed/" + videoId);
+          document.getElementById("video").style.display = "inherit";
+
+          })
+      });
+  
+
+  
+
+
+
+});
