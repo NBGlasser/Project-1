dc7092b63b6d9454872aee5fad6a9a6f9e42715e
Mel-Marsollier <mmarsollier@outlook.com>
2019-03-11 22:17:41 -0700

    With some java snippets

---
+++ databaseapp.js
@@ -0,0 +1,42 @@
+
+var Usernow = moment().format("DD/MM/YYYY HH:mm:ss");
+
+var lastChange = childSnapshot.val().timeUpdate;
+// UserNow - Last change = minutesSinceLastUpdate
+
+// if > 1440 call on function sam ()
+//
+// or use = moment("").fromNow()
+
+
+function sam ()
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+
+var dateToStore = moment().format("DD/MM/YYYY");
+
+console.log(dateToStore);
+
+var timeToStore = moment(00:00:00).format("HH:mm:ss");
+
+var timeUpdate = dateToStore + timeToStore;
+
+console.log(timeUpdate);
+
+database.ref().set(RecipeOfTheDay);
+//overwrites previous recipe .set
+
+
+
+
+Var lastChange = //snapshot of time in database set
+
+// var displayStartConverted = moment(displayStart, "hh:mm").subtract(1, "years");
+
+// var difference = moment().diff(moment(displayStartConverted), "minutes");
+
+// var remainder = difference % displayFrequency;
+
+// var minUntilTrain = displayFrequency - remainder;
+
+// var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm");
